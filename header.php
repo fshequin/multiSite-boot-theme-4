cbcb08497602e9b9ceca918e228a763ef5ec8512
Fred Shequine <fshequin@gmail.com>
2019-04-08 21:54:40 -0400

    commit like 20
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <!-- Bootstrap -->
     <link href="<?php echo get_template_directory_uri(); ?>/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
